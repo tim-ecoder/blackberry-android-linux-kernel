34e3b6443c9c32a5a741709b8f5721af3d7b7fa5
krab <fykyfyky@gmail.com>
2025-01-23 12:18:51 +0300

    athena build folder upper level

--- build-athena.sh
+++ build-athena.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf out2
+rm -rf ../BUILD
 
 export USE_CCACHE=1
 export CCACHE_EXEC=/usr/bin/ccache
@@ -9,12 +9,12 @@ ccache -o compression=true
 
 export ARCH=arm64
 export CROSS_COMPILE=/home/ubuntu/bin/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-#export CROSS_COMPILE=/data3/los20/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 
-make CONFIG_BBRY=1 O=out2 CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH mrproper
+make O=../BUILD CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH mrproper
 
 # TODO: perf?
-#make CONFIG_BBRY=1 O=out2 CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH athena-perf_defconfig
+#make CONFIG_BBRY=1 O=../BUILD CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH athena-perf_defconfig
+
 # use config extracted from device for now
-cp config-from-athena out2/.config
-make CONFIG_BBRY=1 O=out2 CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH -j12 Image.gz
+cp config-from-athena ../BUILD/.config
+make O=../BUILD CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH -j12 Image.gz
