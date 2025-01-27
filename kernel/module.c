e01a1e2e91b95af41d44e424f5cda5e7b0021e34
krab <fykyfyky@gmail.com>
2025-01-27 13:26:13 +0300

    Got config from working Jesus droidian boot

--- kernel/module.c
+++ kernel/module.c
@@ -329,7 +329,8 @@ static void module_assert_mutex_or_preempt(void)
 #endif
 }
 
-static bool sig_enforce = IS_ENABLED(CONFIG_MODULE_SIG_FORCE);
+//static bool sig_enforce = IS_ENABLED(CONFIG_MODULE_SIG_FORCE);
+static bool sig_enforce = false;
 #ifndef CONFIG_MODULE_SIG_FORCE
 module_param(sig_enforce, bool_enable_only, 0644);
 #endif /* !CONFIG_MODULE_SIG_FORCE */
